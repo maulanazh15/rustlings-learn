d8da20f54d13ef519d65e5cf6be978a594b1d90f
maulanazh15 <maulanazh15@gmail.com>
2025-11-04 19:17:44 +0700

    trait dan quiz3 done, tinggal materi lifetimes

--- exercises/15_traits/traits4.rs
+++ exercises/15_traits/traits4.rs
@@ -10,8 +10,13 @@ struct OtherSoftware;
 impl Licensed for SomeSoftware {}
 impl Licensed for OtherSoftware {}
 
-// TODO: Fix the compiler error by only changing the signature of this function.
-fn compare_license_types(software1: ???, software2: ???) -> bool {
+// verbose syntax
+// fn compare_license_types<T: Licensed, U: Licensed>(software1: T, software2: U) -> bool {
+//     software1.licensing_info() == software2.licensing_info()
+// }
+
+// sugar syntax
+fn compare_license_types(software1: impl Licensed, software2: impl Licensed) -> bool {
     software1.licensing_info() == software2.licensing_info()
 }
 
